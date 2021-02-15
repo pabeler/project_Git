57a1450921e8e7b3e8bd53f1bce12edd78c88ba9
Paweł Olejnik <pabeler@o2.pl>
2021-02-15 13:38:48 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/project_Git.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_Git.dir/main.cpp.obj"
+  "project_Git.exe"
+  "project_Git.exe.manifest"
+  "project_Git.lib"
+  "project_Git.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_Git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
